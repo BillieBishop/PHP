ce70b01da4a0c9e76cdb33d8ac298edaa1dbaaf9
Nathalie\Nathalie Desrosiers <n.desrosiers@yahoo.com>
2016-09-23 10:57:07 -0400

    no message

--- blog/articleaddedit.php
+++ blog/articleaddedit.php
@@ -1,15 +1,91 @@
 <?php
+
 require_once 'db.php';
 
-if (!isset($_SESSION['user'])){
+$authorID = $_SESSION['user']['name'];
+echo date("d-m-Y");
+//date_default_timezone_set('America/New_York');
+//$pubDate = $_POST['pubdate'];
+$pubDate = date("Y-m-d");
+
+//$pubDate = new DateTime();
+//$pubDate->setTimestamp($timestamp);
+//var_dump($pubDate->format('d-m-Y H:i:s e'));
+
+function getArticleAddEditForm($authorID, $pubdate, $title = '', $body = '') {
+    $f = <<< ENDTAG
+<h3>Post your article</h3>
+<form method="POST" enctype="multipart/form-data">
+    Author:<input type="text" name="author" value="$authorID"><br><br>
+    Date:<input type="date" name="pubdate" value="date"><br><br>
+    Title:<input type="text" name="title" value = "$title"><br><br>
+    Article:<textarea rows="4" cols="50" name="body" value = "$body"></textarea><br><br>
+    Select image to upload:<input type="file" name="fileToUpload">    
+    <input type="submit" value="Submit">
+</form>
+ENDTAG;
+    return $f;
+}
+
+if (!isset($_SESSION['user'])) {
     echo '<h1>Access forbidden</h1>';
     echo "Only logged in users are allowed to post.";
     echo '<a href="index.php">Click to contunue</a>';
     exit();
+} else {
+    if (!isset($_POST['title'])) {
+        echo getArticleAddEditForm($authorID, $pubDate);
+    } else {
+        //Receiving a submission
+        //$pubDate = $_POST['pubdate'];
+        //$pubDate = new DateTime();
+        //$pubDate->setTimestamp($timestamp);
+        //var_dump($pubDate->format('d-m-Y H:i:s e'));
+
+        $title = $_POST['title'];
+        $body = $_POST['body'];
+
+        $errorList = array();
+
+        //Require subject at least 4 characters long
+        if (strlen($title) < 4) {
+            array_push($errorList, "Subject must be at least 4 characters long");
+        }
+
+        //Require body at least 50 characters long
+        if (strlen($body) < 50) {
+            array_push($errorList, "Article must be at least 50 characters long");
+        }
+
+        //check if array of errors is empty
+        if ($errorList) {
+            //State 3: submission failed - problem found
+            echo "<h5>Problems found in your submission</h5>\n";
+            echo "<ul>\n";
+            foreach ($errorList as $error) {
+                echo "<li>" . htmlspecialchars($error) . "</li>";
+            }
+            echo "</ul>\n";
+            //Make sure you re-display subject and body if submission has failed
+            echo getArticleAddEditForm($authorID, $pubdate, $title, $body);
+        } else {
+            //State 2: submission successful
+            $sql = sprintf("INSERT INTO articles VALUES (NULL, '%s','%s', '%s', '%s')", mysqli_escape_string($conn, $authorID), mysqli_escape_string($conn, $pubDate), mysqli_escape_string($conn, $title), mysqli_escape_string($conn, $body));
+            $result = mysqli_query($conn, $sql);
+            if (!$result) {
+                die("Error executing query [$sql] : " . mysqli_error($conn));
+            } else {
+                echo "Submission successful.";
+                echo getArticleAddEditForm($authorID, $pubDate);
+            }
+        }
+    }
 }
 
+
+
 //TODO: Add 3 state form with Subject and body (text area)
-//Require subject atr least 4 characters long
+//Require subject at least 4 characters long
 //Require body at least 50 characters long
 //Make sure you re-display subject and body if submission has failed.
 //On successful submission, add article to database.
--- blog/register.php
+++ blog/register.php
@@ -1,25 +1,29 @@
 <?php
+
 require_once 'db.php';
+
 //heredoc
-function getRegisterForm($nn = '', $mm = '', $p1 = '', $p2 = '') {
+function getRegisterForm($name = '', $email = '') {
     $f = <<< ENDTAG
 <h3>Register user</h3>
 <form method="POST">
-    Name:<input type="text" name="name" value="$nn"><br><br>
-    Email:<input type="text" name="email" value="$mm"><br><br>
-    Password:<input type="password" name="password" value="$p1"><br><br>
-    Password(retype):<input type="password" name="password2" value="$p2"><br><br>        
+    Name:<input type="text" name="name" value="$name"><br><br>
+    Email:<input type="text" name="email" value="$email"><br><br>
+    Password:<input type="password" name="password"><br><br>
+    Password(retype):<input type="password" name="password2"><br><br>        
     <input type="submit" value="Register">
 </form>
 ENDTAG;
     return $f;
 }
+
 //Tri-state form
 //1-Show
 //2-Submission successful
 //3-Submission failed (show error list and form again)
 
 if (!isset($_POST['name'])) {
+    //State 1: first show
     echo getRegisterForm();
 } else {
     //Receiving a submission
@@ -28,6 +32,7 @@ if (!isset($_POST['name'])) {
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
 
+    //submission received - verify
     $errorList = array();
 
     //name check: 4 characters minimum
@@ -40,62 +45,57 @@ if (!isset($_POST['name'])) {
         array_push($errorList, "Email does not look like a valid email.");
     } else {
         //email check: existing email from database    
-        $query = "SELECT ID FROM users WHERE email = '$email'";
+        $query = sprintf("SELECT ID FROM users WHERE email = '%s'", mysqli_escape_string($conn, $email));
         $result = mysqli_query($conn, $query);
         if (!$result) {
             die("Error executing query: " . mysqli_error($result));
-        } else {
-            $numOfRows = mysqli_num_rows($result);
-            if ($numOfRows == 1) {
-                array_push($errorList, "You are already registered.");
-            }
         }
-        //password check: 8 caracters minimum
-        if (strlen($name) < 8) {
-            array_push($errorList, "Password must be at least 8 characters long.");
-        }else{
-
+        if (mysqli_num_rows($result) != 0) {
+            array_push($errorList, "Email already registered.");
+        }
+    }
+    //password check: 8 caracters minimum
+    if (strlen($password) < 8) {
+        array_push($errorList, "Password must be at least 8 characters long.");
+    } else {
         //password check: containing at least 1 lowercase, 1 uppercase, 1 number and 1 special character       
         $regexPassword = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!#$%^&*()+=<>?]).+$/';
         if (!preg_match($regexPassword, $password)) {
             array_push($errorList, "Password must contains at least 1 lowercase, 1 uppercase, 1 number and 1 specialcharacter.");
-        }
-
-        //password check: password2 the same as first password
-        else if (!$password2 === $password) {
+        }else if (!$password2 === $password) {
+        //password check: password2 the same as first password        
             array_push($errorList, "Both passwords must be the same.");
         }
-        }
+    }
 
-        //email check: existing email from database    
-        $query = "SELECT email FROM users WHERE email = '$email'";
-        $result = mysqli_query($conn, $query);
-        $numOfRows = mysqli_num_rows($result);
-        if ($numOfRows == 1) {
-            echo"You are already registered.";
-            echo getRegisterForm();
+    //email check: existing email from database    
+    $query = "SELECT email FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $query);
+    $numOfRows = mysqli_num_rows($result);
+    if ($numOfRows == 1) {
+        echo"You are already registered.";
+        echo getRegisterForm();
+    }
+    //we check if array of errors is empty (so we validated all) empty array=false
+    if ($errorList) {
+        //State 3: submission failed - problem found
+        echo "<h5>Problems found in your submission</h5>\n";
+        echo "<ul>\n";
+        foreach ($errorList as $error) {
+            echo "<li>" . htmlspecialchars($error) . "</li>";
+        }
+        echo "</ul>\n";
+        echo getRegisterForm($name, $email);
+    } else {
+        //State 2: submission successful
+        $sql = sprintf("INSERT INTO users VALUES (NULL, '%s', '%s', '%s')", mysqli_escape_string($conn, $name), mysqli_escape_string($conn, $email), mysqli_escape_string($conn, $password));
+        $result = mysqli_query($conn, $sql);
+        if (!$result) {
+            die("Error executing query [$sql] : " . mysqli_error($conn));
         } else {
-            //we check if array of errors is empty (so we validated all) empty array=false
-            if ($errorList) {
-                //State 3: submission failed - problem found
-                echo "<h5>Problems found in your submission</h5>\n";
-                echo "<ul>\n";
-                foreach ($errorList as $error) {
-                    echo "<li>" . htmlspecialchars($error) . "</li>";
-                }
-                echo "</ul>\n";
-                echo getRegisterForm($name, $email, $password, $password2);
-            } else {
-                //State 2: submission successful
-                $sql = sprintf("INSERT INTO users VALUES (NULL, '%s', '%s', '%s')", mysqli_escape_string($conn, $name), mysqli_escape_string($conn, $email), mysqli_escape_string($conn, $password));
-                $result = mysqli_query($conn, $sql);
-                if (!$result) {
-                    die("Error executing query [$sql] : " . mysqli_error($conn));
-                } else {
-                    echo "Submission successful.";
-                    echo getRegisterForm();
-                }
-            }
+            echo "Submission successful.";
+            echo getRegisterForm();
         }
     }
 }
+    
